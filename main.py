e29ad648da75c485e965a4808cba29fb140468aa
Juliana Aigueira <121833579+JulianaAigueira@users.noreply.github.com>
2023-04-12 14:56:31 -0300

    templates
    criei barra de navegação usando bootstrap

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+lista_usuarios = ['Lira', 'João', 'Maria', 'Juliana', 'Lucas', 'Julia']
+
 @app.route("/")# atribui uma nova funcionalidade na função a baixo
 def home():
     return render_template('home.html')
@@ -9,6 +11,11 @@ def home():
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
+
+@app.route('/usuarios')
+def usuarios():
+    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
