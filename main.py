dad40939fb5cfc51760b3e51f98157d5f3b1061e
Juliana Aigueira <121833579+JulianaAigueira@users.noreply.github.com>
2023-04-13 15:33:34 -0300

    mensagem
    usei bootstrap pra mensagem na hora de criar conta e fazer login

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormLogin, FormcriarConta
 
 app = Flask(__name__)
@@ -23,6 +23,13 @@ def usuarios():
 def login():
     form_login = FormLogin()
     form_criarconta = FormcriarConta()
+
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success') #exibir msg de login bem sucedido
+        return redirect(url_for('home')) #fez login com sucesso
+    if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        flash(f'Conta criada para o e-mail: {form_criarconta.email.data}', 'alert-success') #criou conta com sucesso
+        return redirect(url_for('home'))  # fez login com sucesso
     return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
 
 
