413f6a0f2c94d21670e80d9b79e4cbabb552efc4
Juliana Aigueira <121833579+JulianaAigueira@users.noreply.github.com>
2023-04-19 08:51:39 -0300

    Update models.py
    criando tabela do post dos usuários no db

--- models.py
+++ models.py
@@ -7,10 +7,12 @@ class usuario(database.Model):
     email = database.Column(database.String, nullable=False, unique=True)
     senha = database.Column(database.String, nullable=False)
     foto_perfil =database.Column(database.String, default='default.jpg')
-
+    posts = database.relationship('Post', backref='autor', lazy=True)
+    
 
 class Post(database.Model):
     id = database.Column(database.Integer, primary_key=True)
     titulo = database.Column(database.String, nullable=False)
     corpo = database.Column(database.Text, nullable=False)
     data_criacao = database.Column(database.DateTime, nullble=False, default=datetime.utcnow)
+    id_usiario = database.Column(database.Integer, database.ForeignKey('usuario.id', nullable=False))
